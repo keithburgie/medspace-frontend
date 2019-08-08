43596878ffd30233b0592a37376770f7827cb621
keithburgie <krburgie@gmail.com>
2019-08-08 16:42:51 -0400

    Front end file structure setup complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import assets from './routes'
+import logo from '../src/assets/images/logo.svg';
+import '../src/assets/styles/App.scss';
 
 function App() {
   return (
@@ -10,14 +11,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

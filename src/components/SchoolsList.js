c026afe675cd8ba0807407d1c54d89d6d0b5f60e
keithburgie <krburgie@gmail.com>
2019-08-15 13:34:02 -0400

    Add and Delete school working. Todos work but not on first load. Async fetch issue.

--- src/components/SchoolsList.js
+++ src/components/SchoolsList.js
@@ -15,7 +15,7 @@ const schoolsUrl = `${api}/schools`
 const todosUrl = `${api}/todos`
 const userSchoolsUrl = `${api}/user_schools`
 
-const user_id = 2
+const user_id = 1
 // const schoolId = 2
 // const schoolsLimit = `?limit=10`
 // const schoolsUrl = `${api}/schools${schoolsLimit}`
@@ -43,7 +43,6 @@ class SchoolsList extends Component {
   // }
 
   componentDidMount() {
-
     Promise.all([
       fetch(schoolsUrl), 
       fetch(userSchoolsUrl)
@@ -52,73 +51,83 @@ class SchoolsList extends Component {
       return Promise.all([all_schools_data.json(), user_schools_data.json()])
     })
     .then(([all_schools_data, user_schools_data]) => {
-
       const user_schools = user_schools_data.filter(user_school => {
         return user_school.user_id === user_id
       })
-
-      // const user_schools = all_schools_data.filter(school => {
-      //   return school.user_schools.find(us => us.user_id === user_id)
-      // })
-
-      // const all_schools = all_schools_data.filter(school => {
-      //   return !school.user_schools.find(us => us.user_id === user_id)
-      // })
-
       this.setState({
         user_schools: [...this.state.user_schools, ...user_schools],
         all_schools: [...this.state.all_schools, ...all_schools_data]
       })
     })
-
-
- 
-
-      // Create array of all todos from user's schools
-      // let user_todos = user_schools.map(todo => todo.todos).flat()
-
-      // // Push data into state
-      // this.setState({
-      //   loading: false,
-      //   all_schools: [...this.state.all_schools, ...all_schools],
-      //   user_schools: [...this.state.user_schools, ...user_schools],
-      //   user_todos: [...this.state.user_todos, ...user_todos]
-      // })
   }
 
+  addSchool = (user_id) => {
+    const new_school_id = this.state.inputValue
 
-  handleAddSchool = (user_id) => {
-    const {user_schools, inputValue} = this.state
-    console.log("Add a school!")
-    
-    let new_school_id = inputValue // the id number
-
-    //this.createDefaultTodos(new_school_id)
-
-    const payload = {
-      user_school: {
-        school_id: new_school_id,
-        user_id: user_id
-      }
-    }
-
+    // Post the new UserSchool
     fetch(userSchoolsUrl, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-    .then(resp => resp.json())
-    .then(new_user_school => {
-      console.log("new_user_school = ", new_user_school.school_id)
-      console.log("inputValue = ", inputValue)
-      this.setState({ 
-        user_schools: [...user_schools, new_user_school],
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        school_id: new_school_id,
+        user_id: user_id
       })
-    }).catch(error => console.log(error))
+    })
+    // Get the ID of the new UserSchool
+    .then(r => r.json()).then(new_user_school => {
+      
+      // Create new Todos with that ID
+      this.createDefaultTodos(new_user_school.id)
+
+      // TODO: FIGURE OUT HOW TO FETCH *AFTER* ALL TODOS HAVE POSTED
+      fetch(`${userSchoolsUrl}/${new_user_school.id}`)
+      .then(r => r.json()
+      .then(user_school => {
+        console.log("user_school = ", user_school)
+        this.setState({
+          user_schools: [...this.state.user_schools, new_user_school]
+        })
+      })).catch(error => console.log(error))
+    })
   }
 
+  deleteSchool = (e) => {
+    const id = parseInt(e.target.dataset.id)
+    fetch(`${userSchoolsUrl}/${id}`, {method: 'DELETE'})
+    .then(r => r.json())
+    .then(this.setState({
+      user_schools: [...this.state.user_schools.filter(user_school => {
+        return user_school.id !== id
+      })]
+    })).catch(error => console.log(error))
+  }
+
+  createDefaultTodos = (user_school_id) => {
+    const tasks = [
+      "Request Recs", "Send Recs", "Send Essay", "Follow Up", 
+      "Send Secondary", "Interview", "Send Thank Yous"
+    ]
+    tasks.map(task => this.addTodo(task, user_school_id))
+  }
+
+  addTodo = (task, user_school_id) => {
+    let todo = {
+      user_school_id: user_school_id,
+      task: task,
+      done: false,
+      note: "",
+      due: new Date().toISOString()
+    }
+
+    fetch(todosUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(todo)
+    })
+    .then(r => r.json())
+    .then(todo => {console.log("Todo Added: ", todo)})
+    .catch(error => console.log(error))
+  }
 
   renderSchool = user_school => {
     const {all_schools} = this.state
@@ -137,53 +146,6 @@ class SchoolsList extends Component {
         selectSchool={this.selectSchool} 
       />
     )
-  }
-
-  // TODO: Delete doesn't work if school was just added
-  //    because school.user_schools[0].id is empty
-  // TODO: Assign ownership of todos to UserSchool and
-  //    dependent destroy them when school is deleted
-  deleteSchool = (e) => {
-    const id = parseInt(e.target.dataset.id)
-    fetch(`${userSchoolsUrl}/${id}`, {method: 'DELETE'})
-    .then(resp => resp.json())
-    .then(this.setState({
-      user_schools: [...this.state.user_schools.filter(user_school => user_school.id !== id)]
-    }))
-    .catch(error => console.log(error))
-  }
-
-
-  createDefaultTodos = (schoolId) => {
-    const tasks = [
-      "Request Recs", "Send Recs", "Send Essay", "Follow Up", 
-      "Send Secondary", "Interview", "Send Thank Yous"
-    ]
-    tasks.map(task => this.addTodo(task, schoolId))
-  }
-
-  addTodo = (task, schoolId) => {
-    let todo = {
-      user_id: user_id,
-      school_id: schoolId,
-      task: task,
-      done: false,
-      note: "",
-      due: new Date().toISOString()
-    }
-
-    fetch(todosUrl, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(todo)
-    })
-    .then(resp => resp.json)
-    .then(todo => {
-      this.setState({
-        user_todos: [...this.state.user_todos, todo]
-      })
-    })
-    .catch(error => console.log(error))
   }
 
   selectSchool = id => {
@@ -216,51 +178,41 @@ class SchoolsList extends Component {
     })
   }
 
-  fadeIn = (el) => {
-    el.classList.add('fadeIn');
-    el.classList.remove('fadeOut');  
-  }
-  
-  fadeOut = (el) => {
-    el.classList.add('fadeOut');
-    el.classList.remove('fadeIn');
-  }
-
   render() {
     // let {user_schools, searchedSchool, selectedSchool, all_schools} = this.state
     let {user_schools, all_schools, selectedSchool} = this.state
     let school = user_schools.find(school => school.id === selectedSchool)
 
     // return (
-    //   // Show call schools if no selected school : show only selection if made
-    //   // <div id="school-list" className={styles.container}>
-    //   <Fragment>
-    //     {
-    //       this.state.loading === true 
-    //       ? 
-    //       <Fade in={this.state.fadeIn}>
-    //         <Spinner color="info" />
-    //       </Fade>
-    //       : 
-    //       <Fragment>
-    //         {/* <Fade in={this.state.fadeIn}> */}
-    //           <InputGroup className={styles.selectInput}>
-    //             <Select 
-    //               className={styles.searchInput} 
-    //               options={all_schools.map(school => {
-    //                 return Object.assign({value: school.id, label: school.name})
-    //               })} 
-    //               onChange={this.handleSelectChange.bind(this)} 
-    //             />
-    //             <Button color="success" onClick={() => this.handleAddSchool(user_id)}>Add School</Button>
-    //           </InputGroup>
-    //         {/* </Fade> */}
+      //   // Show call schools if no selected school : show only selection if made
+      //   // <div id="school-list" className={styles.container}>
+      //   <Fragment>
+      //     {
+      //       this.state.loading === true 
+      //       ? 
+      //       <Fade in={this.state.fadeIn}>
+      //         <Spinner color="info" />
+      //       </Fade>
+      //       : 
+      //       <Fragment>
+      //         {/* <Fade in={this.state.fadeIn}> */}
+      //           <InputGroup className={styles.selectInput}>
+      //             <Select 
+      //               className={styles.searchInput} 
+      //               options={all_schools.map(school => {
+      //                 return Object.assign({value: school.id, label: school.name})
+      //               })} 
+      //               onChange={this.handleSelectChange.bind(this)} 
+      //             />
+      //             <Button color="success" onClick={() => this.addSchool(user_id)}>Add School</Button>
+      //           </InputGroup>
+      //         {/* </Fade> */}
 
-    //         {/* {selectedSchool === null ? user_schools.map(i => this.renderSchool(i)) : this.renderSchool(school)} */}
-    //         {this.state.user_schools.map(school => this.renderSchool(school))}
-    //       </Fragment>
-    //     }
-    //   </Fragment>
+      //         {/* {selectedSchool === null ? user_schools.map(i => this.renderSchool(i)) : this.renderSchool(school)} */}
+      //         {this.state.user_schools.map(school => this.renderSchool(school))}
+      //       </Fragment>
+      //     }
+      //   </Fragment>
     // )
     return (
       <Fragment>
@@ -269,7 +221,7 @@ class SchoolsList extends Component {
           options={all_schools.map(school => Object.assign({value: school.id, label: school.name}))} 
           onChange={this.handleSelectChange.bind(this)} 
         />
-        <Button color="success" onClick={() => this.handleAddSchool(user_id)}>Add School</Button>
+        <Button color="success" onClick={() => this.addSchool(user_id)}>Add School</Button>
 
         {user_schools.map(school => this.renderSchool(school))}
 
